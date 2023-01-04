03fe59cb558ac8a2a0fa581edace68b364dece62
saiman pokhrel <70017194+WOD-MN@users.noreply.github.com>
2023-01-04 10:32:51 +0545

    Create code.ino

---
+++ code.ino
@@ -0,0 +1,46 @@
+#include <SPI.h>
+#include <LoRa.h>
+#include <WiFi.h>
+#include <GoogleMaps.h>
+
+const char* ssid = "your_wifi_ssid";
+const char* password = "your_wifi_password";
+
+void setup() {
+  // Initialize LoRa radio
+  LoRa.begin(915E6);
+
+  // Connect to WiFi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  
+  // Initialize Google Maps
+  GoogleMaps.begin(map_api_key);
+}
+
+void loop() {
+  // Check for incoming LoRa packets
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    // Read GPS data from packet
+    String data = "";
+    while (LoRa.available()) {
+      data += (char)LoRa.read();
+    }
+    
+    // Split data into latitude and longitude
+    int commaIndex = data.indexOf(',');
+    float lat = data.substring(0, commaIndex).toFloat();
+    float lon = data.substring(commaIndex+1).toFloat();
+    
+    // Plot GPS data on map
+    GoogleMaps.plot(lat, lon);
+  }
+}
